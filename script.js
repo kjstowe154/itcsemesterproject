c71786400961eed337a8669c260318c1b1082516
Matthew Johnson <149624352+Mattoo123@users.noreply.github.com>
2023-12-08 17:33:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,42 @@ backToTopButton.addEventListener('click', function() {
     behavior: 'smooth'
   });
 });
+
+
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+  // Array of video game names
+  const games = ['MushroomrGal', 'Monster Rhythm', 'Throne of Shadows', 'Code Sculpt'];
+
+  // Cost per game
+  const costPerGame = 50;
+
+  // Target the games list and total cost elements
+  const gamesList = document.getElementById('games-list');
+  const totalCostElement = document.getElementById('total-cost');
+
+  // Iterate through the games array to display names
+  games.forEach((game) => {
+    // Create list item for each game
+    const listItem = document.createElement('li');
+    listItem.textContent = game;
+
+    // Add the item to the games list
+    gamesList.appendChild(listItem);
+  });
+
+  // Iterate again to add the price next to each game
+  games.forEach((game) => {
+    // Find the list item corresponding to the game
+    const listItem = Array.from(gamesList.children).find(item => item.textContent === game);
+
+    // Add the price next to the game name
+    listItem.textContent += ` - $${costPerGame}`;
+  });
+
+  // Calculate the total cost (cost per game multiplied by the number of games)
+  const totalCost = costPerGame * games.length;
+
+  // Display the total cost
+  totalCostElement.textContent = totalCost;
+});
